92822a4b3085db2047ac0689952aa1a793b57840
Malakk <malak.m.abdelwahab05@gmail.com>
2026-02-08 19:14:20 +0200

    pls pls work this time

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,8 +3,12 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import call from "../images/call.png"
+import { useForm, ValidationError } from '@formspree/react';
 
 function Contact(){
+
+    const [state, handleSubmit] = useForm("xpqjzvez");
+
     const formInitialDetails = {
         firstName: '',
         lastName: '',
@@ -72,14 +76,14 @@ function Contact(){
                                     <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate("lastName", e.target.value)}></input>
                                 </Col>
                                 <Col sm={6} className="px-1">
-                                    <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate("email", e.target.value)}></input>
+                                    <input type="email" id="email" name="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate("email", e.target.value)}></input>
                                 </Col>
                                 <Col sm={6} className="px-1">
                                     <input type="tel" value={formDetails.phone} placeholder="Phone Number" onChange={(e) => onFormUpdate("phone", e.target.value)}></input>
                                 </Col>
                                 <Col>
-                                    <textarea rows={6} value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate("message", e.target.value)}></textarea>
-                                    <button type="submit"><span>{buttonText}</span></button>
+                                    <textarea rows={6} id="message" name="message" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate("message", e.target.value)}></textarea>
+                                    <button type="submit" disabled={state.submitting}><span>{buttonText}</span></button>
                                 </Col>
                                 {
                                     status.message &&
