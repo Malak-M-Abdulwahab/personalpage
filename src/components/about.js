d414c51f653e100e4bef317fa75259516c1bc8b7
Malakk <malak.m.abdelwahab05@gmail.com>
2026-02-10 22:31:58 +0200

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -11,7 +11,7 @@ function About(){
                     <Col>
                         <h2>About Me</h2>
                         <img src={me} alt='malak moustafa'></img>
-                        <p>I am a 4th year student at University of Alexandria, Faculty of Engineering, currently majoring in Computer and Communications engineering.
+                        <p>I am a 4th year student at the University of Alexandria, Faculty of Engineering, currently majoring in Computer and Communications engineering.
                             My mission is to prove that femininity doesn't get in the way of professionalism, but strengthens it into a more complete and whole concept that's inclusive and balanced.<br />
                             I am passionate about making platforms that are easy on the eyes, as well as productive and useable.
                             My skills continue to grow and I always look for more opportunities to improve and become not just a better developer, but a better person as well.</p>
